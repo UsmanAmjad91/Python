7dea95726807c66c866b978872082fd9e79ba424
UsmanAmjad91 <usmanamjad187@gmail.com>
2025-06-20 21:08:54 -0700

    first commit

---
+++ 1-Fundamentals/function.py
@@ -0,0 +1,6 @@
+def python_function():
+    print("I am a Python function")
+    pyVar = "This declares a new Python variable"
+    print(pyVar)
+
+python_function()
---
+++ 1-Fundamentals/variable.py
@@ -0,0 +1,34 @@
+x = 12.2
+y = 7
+z = 9.0
+
+print(x)
+print(y)
+print(z)
+
+
+# Primitive data types in Python
+name="Usman"
+age=30
+cash=100.50
+retired=False
+print ("Data type of the variable 'name' is ", type(name))
+print ("Data type of the variable 'age' is ", type(age))
+print ("Data type of the variable 'cash' is ", type(cash))
+print ("Data type of the variable 'retired' is ", type(retired))
+
+# """
+# Composite data types:
+# List, Dictionary, Tuple, Set
+# """
+print("THIS IS A BREAK TO THE NEXT SECTION OF LESSON..............")
+nucamp_locations = ["Seattle", "Tacoma", "Bellevue"]  # Storing a List
+Usman_Info = {"name": "Usman", "age": 30, "cash": 100.25,
+            "retired": False}  # Storing a Dictionary
+my_tuple = ("apple", "banana", "cherry")  # Storing a Tuple
+my_set = {"cats", "dogs", "birds"}  # Storing a Set
+
+print("Data type of the variable 'nucamp_locations' is", type(nucamp_locations))
+print("Data type of the variable 'Usman' is", type(Usman_Info))
+print("Data type of the variable 'my_tuple' is", type(my_tuple))
+print("Data type of the variable 'my_set' is", type(my_set))
